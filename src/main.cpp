2951c65d0b59092b02c0b4f701cadfab1e745805
Marc Racicot <racicotm@gmail.com>
2016-10-05 15:11:34 +0000

    compile & link simple program

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,19 @@
 #include <NVX/nvx_timer.hpp>
 
 #include "NVXIO/FrameSource.hpp"
-//#include "NVXIO/Render.hpp"
-//#include "NVXIO/Application.hpp"
-//#include "NVXIO/Utility.hpp"
+#include "NVXIO/Render.hpp"
+#include "NVXIO/Application.hpp"
+#include "NVXIO/Utility.hpp"
 
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
+
+    nvxio::Application &app = nvxio::Application::get();
+
     return 0;
 }
+
+
 
